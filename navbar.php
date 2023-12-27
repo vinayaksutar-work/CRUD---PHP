ccedd573ac487a73b33adc78494f07897f52b6c5
vinayaksutar-work <vinayaksutar.work@gmail.com>
2023-12-27 22:57:11 +0530

    make nav item dynamic

--- navbar.php
+++ navbar.php
@@ -2,19 +2,19 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+  <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home</a>
+      <li class="nav-item <?php if($title == 'Home'){ echo 'active';} ?>">
+        <a class="nav-link" href="index.php">HOME</a>
+      </li>
+      <li class="nav-item <?php if($title == 'View'){ echo 'active';} ?>">
+        <a class="nav-link" href="view.php">VIEW</a>
+      </li>
+      <li class="nav-item <?php if($title == 'Update'){ echo 'active';} ?>">
+        <a class="nav-link" href="view.php">UPDATE</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="view.php">View</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="view.php">Update</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="view.php">Delete</a>
+        <a class="nav-link" href="view.php">DELETE</a>
       </li>
     </ul>
   </div>
